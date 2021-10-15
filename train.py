171456110f3bdd796c951d6230310c6aa6f4e1ca
kirill <donthaveone>
2021-10-15 09:31:46 +0300

    imitate big batch and update gates from first epoch

--- train.py
+++ train.py
@@ -95,13 +95,13 @@ device = torch.device("cuda")
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!")
   # dim = 0 [30, xxx] -> [10, ...], [10, ...], [10, ...] on 3 GPUs
-  model = nn.DataParallel(model,device_ids=[0,1]).cuda()
+  model = nn.DataParallel(model, device_ids=[0, 1]).cuda()
 model.to(device)
-
-criterion = LogNLLLoss()
+print(f"After only defining the model {torch.cuda.memory_allocated()} of GPU is occupied")
+criterion = lib.criterions.iou_approximation # LogNLLLoss()
 optimizer = torch.optim.Adam(list(model.parameters()), lr=args.learning_rate,
                              weight_decay=1e-5)
-
+softmax = nn.Softmax(dim=1)
 
 pytorch_total_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
 print("Total_params: {}".format(pytorch_total_params))
@@ -109,57 +109,63 @@ print("Total_params: {}".format(pytorch_total_params))
 seed = 3000
 np.random.seed(seed)
 torch.manual_seed(seed)
+
 torch.cuda.manual_seed(seed)
+
 # torch.set_deterministic(True)
 # random.seed(seed)
 
-
+train_loss = []
+validate_loss = []
 for epoch in range(args.epochs):
 
     epoch_running_loss = 0
     
-    for batch_idx, (X_batch, y_batch, *rest) in enumerate(dataloader):        
+    for batch_idx, (X_batch, y_batch, rest) in enumerate(dataloader):
         
-        
+        X_batch = Variable(X_batch.to(device='cuda')) # cuda
+        y_batch = Variable(y_batch.to(device='cuda')) # cuda
 
-        X_batch = Variable(X_batch.to(device ='cuda'))
-        y_batch = Variable(y_batch.to(device='cuda'))
-        
         # ===================forward=====================
-        
-
         output = model(X_batch)
+        if criterion is not LogNLLLoss:
+            output = softmax(output)[:, 1, :, :]
 
-        tmp2 = y_batch.detach().cpu().numpy()
-        tmp = output.detach().cpu().numpy()
-        tmp[tmp>=0.5] = 1
-        tmp[tmp<0.5] = 0
-        tmp2[tmp2>0] = 1
-        tmp2[tmp2<=0] = 0
-        tmp2 = tmp2.astype(int)
-        tmp = tmp.astype(int)
-
-        yHaT = tmp
-        yval = tmp2
-
-        
-
-        loss = criterion(output, y_batch)
-        
         # ===================backward====================
-        optimizer.zero_grad()
+        loss = criterion(output, y_batch)
         loss.backward()
-        optimizer.step()
+
+        if (batch_idx + 1) % 5 == 0:
+            optimizer.step()
+            optimizer.zero_grad()
+            print(f"Batch #{batch_idx + 1}")
+            
         epoch_running_loss += loss.item()
+        
+        if (batch_idx + 1) % 20 == 0:
+            print("Train Loss = {:.4f}".format(epoch_running_loss/(batch_idx + 1)))
+            with open(direc + "train_loss.txt", 'a') as tl:
+                tl.write(str(epoch_running_loss/(batch_idx + 1)))
+                tl.write("\n")
+            fulldir = direc + "{}/".format(epoch)
+            if not os.path.isdir(fulldir):
+                os.makedirs(fulldir)
+            
+            fulldir = direc + "{}/{}/".format(epoch, batch_idx + 1)
+            if not os.path.isdir(fulldir):
+                os.makedirs(fulldir)
+            
+            torch.save(model.state_dict(), fulldir + args.modelname + ".pth")
         
     # ===================log========================
     print('epoch [{}/{}], loss:{:.4f}'
           .format(epoch, args.epochs, epoch_running_loss/(batch_idx+1)))
-
+    train_loss.append(epoch_running_loss/(batch_idx+1))
+    del X_batch, y_batch, output
     
-    if epoch == 10:
-        for param in model.parameters():
-            param.requires_grad =True
+    for param in model.parameters():
+        param.requires_grad = True
+
     if (epoch % args.save_freq) ==0:
 
         for batch_idx, (X_batch, y_batch, *rest) in enumerate(valloader):
