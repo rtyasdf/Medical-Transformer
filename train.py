086128e316dc0e662adabde5b879a806d49841a4
kirill <donthaveone>
2021-10-15 09:27:00 +0300

    reorder some lines

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 # Code for MedT
 
-import torch
 import lib
 import argparse
 import torch
@@ -17,10 +16,9 @@ import torch.utils.data as data
 from PIL import Image
 import numpy as np
 from torchvision.utils import save_image
-import torch
 import torch.nn.init as init
 from utils import JointTransform2D, ImageToImage2D, Image2D
-from metrics import jaccard_index, f1_score, LogNLLLoss,classwise_f1
+from metrics import jaccard_index, f1_score, LogNLLLoss, classwise_f1
 from utils import chk_mkdir, Logger, MetricList
 import cv2
 from functools import partial
@@ -44,10 +42,7 @@ parser.add_argument('--weight-decay', '--wd', default=1e-5, type=float,
                     metavar='W', help='weight decay (default: 1e-5)')
 parser.add_argument('--train_dataset', required=True, type=str)
 parser.add_argument('--val_dataset', type=str)
-parser.add_argument('--save_freq', type=int,default = 10)
-
-parser.add_argument('--modelname', default='MedT', type=str,
-                    help='type of model')
+parser.add_argument('--save_freq', type=int, default = 10)
 parser.add_argument('--cuda', default="on", type=str, 
                     help='switch on/off cuda option (default: off)')
 parser.add_argument('--aug', default='off', type=str,
@@ -58,8 +53,8 @@ parser.add_argument('--save', default='default', type=str,
                     help='save the model')
 parser.add_argument('--direc', default='./medt', type=str,
                     help='directory to save')
-parser.add_argument('--crop', type=int, default=None)
-parser.add_argument('--imgsize', type=int, default=None)
+parser.add_argument('--crop', default=None, type=int)
+parser.add_argument('--imgsize', default=None, type=int)
 parser.add_argument('--device', default='cuda', type=str)
 parser.add_argument('--gray', default='no', type=str)
 
@@ -84,22 +79,18 @@ else:
 
 tf_train = JointTransform2D(crop=crop, p_flip=0.5, color_jitter_params=None, long_mask=True)
 tf_val = JointTransform2D(crop=crop, p_flip=0, color_jitter_params=None, long_mask=True)
-train_dataset = ImageToImage2D(args.train_dataset, tf_train)
+
+train_dataset = ImageToImage2D(args.train_dataset, tf_val)
 val_dataset = ImageToImage2D(args.val_dataset, tf_val)
+
 predict_dataset = Image2D(args.val_dataset)
-dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+
+dataloader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True, num_workers=2)
 valloader = DataLoader(val_dataset, 1, shuffle=True)
 
-device = torch.device("cuda")
 
-if modelname == "axialunet":
-    model = lib.models.axialunet(img_size = imgsize, imgchan = imgchant)
-elif modelname == "MedT":
-    model = lib.models.axialnet.MedT(img_size = imgsize, imgchan = imgchant)
-elif modelname == "gatedaxialunet":
-    model = lib.models.axialnet.gated(img_size = imgsize, imgchan = imgchant)
-elif modelname == "logo":
-    model = lib.models.axialnet.logo(img_size = imgsize, imgchan = imgchant)
+model = lib.models.axialnet.MedT(img_size=imgsize, imgchan=imgchant, groups=4)
+device = torch.device("cuda")
 
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!")
